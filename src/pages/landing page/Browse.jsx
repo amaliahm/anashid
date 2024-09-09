e93aae48ed6aad9326611b023c70f4396418eec8
amaliahm <a.ghandouz@esi-sba.dz>
2024-09-09 23:59:01 +0100

    fix before

--- src/pages/landing page/Browse.jsx
+++ src/pages/landing page/Browse.jsx
@@ -3,12 +3,12 @@ import { browseCircle } from "../../assets/images"
 
 const Browse = () => {
     return (
-      <div className="lg:pt-10 overflow-hidden">  
+      <div className="lg:pt-10 overflow-hidden p-0">  
         <div className="relative">
-          <img src={browseCircle} alt="hero" className="hidden lg:block absolute bottom-0 right-14 w-1/12 h-auto"/>
-          <div className="bg-[rgba(113,60,150,0.24)] sm:m-14 m-24 rounded-2xl sm:p-8 md:p-10 lg:p-12 flex flex-col justify-between items-center">
+          <img src={browseCircle} alt="hero" className="hidden lg:block absolute bottom-0 right-20 w-1/12 h-auto"/>
+          <div className="bg-[rgba(113,60,150,0.24)] rounded-2xl lg:m-20 m-5 mt-0 p-3 pb-6 sm:p-8 md:p-10 lg:p-12 flex flex-col justify-between items-center">
             <Header title='Browse by Language, Theme, or Popularity' />
-            <div className="lg:px-24 md: px-16 px-6 pt-8"> 
+            <div className="lg:px-24 md:px-16 px-6 pt-8"> 
                 Navigate through a vast library of Islamic Anashid by filtering content based on your preferences. Whether you want to listen by language, delve into specific themes, or discover what's trending, our intuitive categorization allows you to easily find the Anashid that resonate with your soul. Experience seamless browsing and find your favorite spiritual melodies in just a few clicks
             </div>
           </div>
--- src/pages/landing page/Genre.jsx
+++ src/pages/landing page/Genre.jsx
@@ -21,7 +21,7 @@ const Genre = () => {
 
     return (
         <>
-          <div className="mt-8 sm:p-8 p-14">
+          <div className="mt-0 p-2 lg:p-14">
             <div className="flex items-center justify-between px-6">
                 <Header title='Genre of Anshid' />
                 <div 
@@ -31,24 +31,25 @@ const Genre = () => {
                     see more
                 </div>
             </div>
-            <div className="py-8 px-2 mt-10 flex justify-between gap-12 items-start">
+            <div className="p-8 mt-10 flex flex-wrap justify-between items-center">
                 {Object.keys(genre).map((e, i) => (
                     <div 
-                      className="w-1/4 sm:min-h-48 md:min-h-64 lg:min-h-[400px] h-fit bg-transparent rounded-xl border-2 border-[var(--mainColor)] flex flex-col justify-evenly items-center pt-2 flex-wrap relative" 
+                      className="w-[28%] min-w-[200px] h-fit bg-transparent rounded-xl border-2 border-[var(--mainColor)] flex flex-col justify-between m-auto mb-7 items-center pt-2 flex-wrap relative" 
                       key={i}
                     >
                         <div 
                           className={'bg-cover bg-center rounded-full w-36 h-36 md:w-48 md:h-48 lg:w-80 lg:h-80 flex justify-center items-center'} 
                           style={{ backgroundImage: `url(${genre[e].image})` }}
                         >
-                            <div className="w-10 h-10 md:w-18 md:h-18 lg:w-26 lg:h-26 bg-[var(--backgroundColor)] rounded-full top-1/4 right-1/4"></div>
+                            <div className="w-14 h-14 bg-[var(--backgroundColor)] rounded-full"></div>
                         </div>
                         <div className="relative text-2xl capitalize font-medium">
                             {genre[e].name}
                         </div>
                         <div 
-                          className={'absolute bg-cover bg-center rounded-full w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24 -bottom-6 right-0 lg:-bottom-14 lg:right-4'} 
-                          style={{ backgroundImage: playIcon }}
+                          className={'hover:cursor-pointer absolute bg-cover bg-center rounded-full w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24 -bottom-6 right-0 lg:-bottom-14 lg:right-4'} 
+                          style={{ backgroundImage: `url(${playIcon})` }}
+                          onClick={() => navigate('/auth/signup')}
                         ></div>
                     </div>
                 ))}
