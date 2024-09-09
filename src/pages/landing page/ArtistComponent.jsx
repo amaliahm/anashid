5b56b1a59ae975c2707b0fc7b452b066e2d8d8b7
amaliahm <a.ghandouz@esi-sba.dz>
2024-09-10 00:49:39 +0100

    add artist

--- src/pages/landing page/ArtistComponent.jsx
+++ src/pages/landing page/ArtistComponent.jsx
@@ -1,6 +1,8 @@
-import { artist1, artist2, artist3, artist4, artist5, artistArrow } from "../../assets/images"
+import { artist1, artist2, artist3, artist4, artist5, artistArrow, playIcon } from "../../assets/images"
+import { useNavigate } from "react-router-dom"
 
 const ArtistComponent = () => {
+    const navigate = useNavigate()
     return (
         <>
           <div className="bg-[var(--thirdColor)] z-18 p-2 mr-3 mt-8 lg:mr-5 rounded-2xl relative">
@@ -9,13 +11,27 @@ const ArtistComponent = () => {
               src={artistArrow}
               alt="artists"
             />
-            <p className={`capitalize font-medium text-white text-2xl lg:text-6xl md:text-5xl text-wrap text-center my-8`}>
+            <p className='capitalize font-medium text-white text-2xl lg:text-6xl md:text-5xl text-wrap text-center my-8'>
               Meet the Voices Behind the Anashid
             </p>
-            <div className="w-11/12 m-auto flex-col lg:flex-row flex justify-between items-center">
-              <img src={artist1} className="w-76 h-76 mb-5"/>
-              <img src={artist2} className="w-76 h-76 mb-5"/>
-              <img src={artist5} className="w-76 h-76 mb-5"/>
+            <div className="w-11/12 m-auto flex-col lg:flex-row flex justify-between items-center relative">
+              <img 
+                src={artist1} 
+                className="w-76 h-76 mb-5"
+              />
+              <img 
+                src={artist2} 
+                className="w-76 h-76 mb-5"
+              />
+              <img 
+                src={artist5} 
+                className="w-76 h-76 mb-5"
+              />
+              <img 
+                src={playIcon} 
+                className="hover:cursor-pointer w-76 h-76 mb-5 absolute right-[46%]" 
+                onClick={() => navigate('/auth/signup')}
+              />
             </div>
           </div>
         </>
