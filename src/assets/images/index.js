251f79339ea88b3d521675784d125b80fade379f
amaliahm <a.ghandouz@esi-sba.dz>
2024-09-09 22:41:27 +0100

    add genre 2

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,7 +4,8 @@ import browseCircle from './browseCircle.svg'
 import genre1 from './genre1.jpg'
 import genre2 from './genre2.png'
 import genre3 from './genre3.png'
+import playIcon from './playIcon.svg'
 
 export {
-    hero, circle, browseCircle, genre1, genre2, genre3
+    hero, circle, browseCircle, genre1, genre2, genre3, playIcon
 }
--- src/pages/landing page/Genre.jsx
+++ src/pages/landing page/Genre.jsx
@@ -1,6 +1,6 @@
 import Header from "../../Components/Header"
 import { useNavigate } from "react-router-dom"
-import { genre1, genre2, genre3 } from "../../assets/images"
+import { genre1, genre2, genre3, playIcon } from "../../assets/images"
 
 const Genre = () => {
     const navigate = useNavigate()
@@ -22,7 +22,7 @@ const Genre = () => {
     return (
         <>
           <div className="mt-8 sm:p-8 p-14">
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between px-6">
                 <Header title='Genre of Anshid' />
                 <div 
                   className="hover:cursor-pointer capitalize text-[var(--mainColor)]"
@@ -31,21 +31,25 @@ const Genre = () => {
                     see more
                 </div>
             </div>
-            <div className="p-12 mt-12 flex justify-between gap-12 items-start">
+            <div className="py-8 px-2 mt-10 flex justify-between gap-12 items-start">
                 {Object.keys(genre).map((e, i) => (
                     <div 
                       className="w-1/4 sm:min-h-48 md:min-h-64 lg:min-h-[400px] h-fit bg-transparent rounded-xl border-2 border-[var(--mainColor)] flex flex-col justify-evenly items-center pt-2 flex-wrap relative" 
                       key={i}
                     >
                         <div 
-                          className={'bg-cover bg-center rounded-full w-36 h-36 md:w-48 md:h-48 lg:w-80 lg:h-80 relative'} 
+                          className={'bg-cover bg-center rounded-full w-36 h-36 md:w-48 md:h-48 lg:w-80 lg:h-80 flex justify-center items-center'} 
                           style={{ backgroundImage: `url(${genre[e].image})` }}
                         >
-                            <div className="absolute w-16 h-16 md:w-20 md:h-20 lg:w-40 lg:h-40 bg-[var(--backgroundColor)] rounded-full top-1/4 right-1/4"></div>
+                            <div className="w-10 h-10 md:w-18 md:h-18 lg:w-26 lg:h-26 bg-[var(--backgroundColor)] rounded-full top-1/4 right-1/4"></div>
                         </div>
                         <div className="relative text-2xl capitalize font-medium">
                             {genre[e].name}
                         </div>
+                        <div 
+                          className={'absolute bg-cover bg-center rounded-full w-12 h-12 md:w-16 md:h-16 lg:w-24 lg:h-24 -bottom-6 right-0 lg:-bottom-14 lg:right-4'} 
+                          style={{ backgroundImage: playIcon }}
+                        ></div>
                     </div>
                 ))}
             </div>
