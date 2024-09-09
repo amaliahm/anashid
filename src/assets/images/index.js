b2f998692a99a292057c81a0335aec14ae0cb470
amaliahm <a.ghandouz@esi-sba.dz>
2024-09-10 00:13:32 +0100

    add create section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,7 +5,9 @@ import genre1 from './genre1.jpg'
 import genre2 from './genre2.png'
 import genre3 from './genre3.png'
 import playIcon from './playIcon.svg'
+import heartCreate from './heartCreate.svg'
+import playlistCreate from './playlistCreate.svg'
 
 export {
-    hero, circle, browseCircle, genre1, genre2, genre3, playIcon
+    hero, circle, browseCircle, genre1, genre2, genre3, playIcon, heartCreate, playlistCreate
 }
--- src/pages/landing page/CreateSection.jsx
+++ src/pages/landing page/CreateSection.jsx
@@ -1,17 +1,22 @@
+import { browseCircle, playlistCreate, heartCreate } from "../../assets/images"
+import Header from "../../Components/Header"
+
 const CreateSection = () => {
     return (
         <>
-          {/* <div className="lg:pt-10 overflow-hidden">  
-          <div className="relative">
-            <img src={browseCircle} alt="hero" className="hidden lg:block absolute bottom-0 right-14 w-1/12 h-auto"/>
-            <div className="bg-[rgba(113,60,150,0.24)] sm:m-14 m-24 rounded-2xl sm:p-8 md:p-10 lg:p-12 flex flex-col justify-between items-center">
-              <Header title='Browse by Language, Theme, or Popularity' />
-              <div className="lg:px-24 md: px-16 px-6 pt-8"> 
-                Navigate through a vast library of Islamic Anashid by filtering content based on your preferences. Whether you want to listen by language, delve into specific themes, or discover what's trending, our intuitive categorization allows you to easily find the Anashid that resonate with your soul. Experience seamless browsing and find your favorite spiritual melodies in just a few clicks
-              </div>
+          <div className="lg:pt-10 overflow-hidden p-0">  
+        <div className="relative">
+          <img src={browseCircle} alt="hero" className="hidden lg:block absolute bottom-0 right-20 w-1/12 h-auto"/>
+          <img src={heartCreate} alt="hero" className="hidden lg:block absolute top-4 right-36 w-1/12 h-auto"/>
+          <img src={playlistCreate} alt="hero" className="hidden lg:block absolute bottom-4 right-80 w-1/12 h-auto"/>
+          <div className="bg-[rgba(113,60,150,0.24)] rounded-2xl lg:m-20 m-5 mt-0 p-2 pb-6 sm:p-8 md:p-10 lg:p-12 flex flex-col justify-between items-start">
+            <Header title='Create Playlists & Save Favorites' />
+            <div className="lg:px-12 px-6 pt-8 lg:w-3/4"> 
+              Enhance your spiritual journey by curating your own playlists or marking Anashid as favorites. Whether you wish to revisit your most cherished songs or craft a playlist for different moods or occasions, our platform gives you the tools to personalize your listening experience. Save your favorites and keep them close at heart, all in one convenient place.
             </div>
           </div>
-        </div> */}
+        </div>
+      </div>
         </>
     )
 }
