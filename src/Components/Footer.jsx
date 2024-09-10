9b68930de82a6f7f82a62e92b156a12227bdd6cd
amaliahm <a.ghandouz@esi-sba.dz>
2024-09-10 01:39:40 +0100

    landing page --d ((1

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,49 +5,49 @@ const Footer = () => {
         <>
           <div className="uppercase p-20 pb-8 lg:px-32 flex lg:flex-row flex-col flex-wrap justify-between items-center gap-5">
             <div className="flex flex-col justify-center items-center gap-5">
-                <p className="text-2xl font-bold uppercase flex flex-row">
+                <p className="hover:cursor-pointer text-2xl font-bold uppercase flex flex-row">
                     company
                 </p>
-                <p>
+                <p className="hover:cursor-pointer">
                     terms & conditions
                 </p>
-                <p>
+                <p className="hover:cursor-pointer">
                     privacy policy
                 </p>
             </div>
             <div className="flex flex-col justify-center items-center gap-5">
-                <p className="text-2xl font-bold uppercase">
+                <p className="hover:cursor-pointer text-2xl font-bold uppercase">
                     menu
                 </p>
-                <p>
+                <p className="hover:cursor-pointer">
                     home
                 </p>
-                <p>
+                <p className="hover:cursor-pointer">
                     about us
                 </p>
-                <p>
+                <p className="hover:cursor-pointer">
                     signup
                 </p>
             </div>
             <div className="flex flex-col justify-center items-center gap-5">
-                <p className="text-2xl font-bold uppercase">
+                <p className="hover:cursor-pointer text-2xl font-bold uppercase">
                     get in touch
                 </p>
-                <div className="flex justify-between items-center gap-2">
+                <div className="flex justify-between items-center gap-6">
                     <img 
                       src={instagram} 
                       alt="instagram" 
-                      className="w-11 h-fit" 
+                      className="hover:cursor-pointer w-11 h-fit" 
                     />
                     <img 
                       src={telegram} 
                       alt="telegram" 
-                      className="w-11 h-fit"
+                      className="hover:cursor-pointer w-11 h-fit"
                     />
                     <img 
                       src={facebook} 
                       alt="facebook" 
-                      className="w-11 h-fit"
+                      className="hover:cursor-pointer w-11 h-fit"
                     />
                 </div>
             </div>
